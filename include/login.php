694a9f398930df4fdfd6a45640ad74744406094a
Masooma <masooma.zahedi1@gmail.com>
2023-05-19 10:49:03 -0500

    fix login.php for href

--- include/login.php
+++ include/login.php
@@ -8,7 +8,7 @@ function user_login($data){
     if(isset($res['username'])){
         if($res['password'] == $data['password']){
             $_SESSION['username'] = $res['name'];
-            header("location:../admin/dashbord.php");
+            header("location:../admin/dashbord.php?m=menu&p=list");
         }
         else{
             header("location:../admin/index.php?login=error");
